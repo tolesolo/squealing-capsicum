500a5e52afba4f5ec7401bc76c5af26178bdb276
tolesolo <blucky78@yahoo.com>
2016-01-25 05:21:10 +0700

    Update faq_page.js

--- app/modules/custom/faq_page/faq_page.js
+++ app/modules/custom/faq_page/faq_page.js
@@ -24,13 +24,13 @@ function faq_page_page() {
 	    		'data-expanded-icon': 'arrow-d' 
 	    	}
 	    },
-	    { header: 'Gimana cara membatalkan order?', content: '<p>Hubungi call center kami. Klik nomor ini <a onclick="javascript:window.open(\'tel:+6281234805101\', \'_system\', \'location=yes\');">08-1234-805-101</a>.</p>', 
+	    { header: 'Gimana cara membatalkan order?', content: '<p>Hubungi call center kami. Klik nomor ini <a onclick="javascript:window.open(\'tel:+6281234560736\', \'_system\', \'location=yes\');">08-123456-OJEG(0736)</a>.</p>', 
 	    	attributes: {
 	    		'data-collapsed-icon': 'arrow-r',
 	    		'data-expanded-icon': 'arrow-d' 
 	    	} 
 	    },
-	    { header: 'Driver Get Tranz mengganggu saya', content: '<p>Hubungi call center kami. Klik nomor ini <a onclick="javascript:window.open(\'tel:+6281234805101\', \'_system\', \'location=yes\');">08-1234-805-101</a>. Atau masukan keluhan di halaman keluhan, kami akan memberikan surat peringatan, jika perlu kami akan bawa ke pihak berwajib.</p>', 
+	    { header: 'Driver Get Tranz mengganggu saya', content: '<p>Hubungi call center kami. Klik nomor ini <a onclick="javascript:window.open(\'tel:+6281234560736\', \'_system\', \'location=yes\');">08-123456-OJEG(0736)</a>. Atau masukan keluhan di halaman keluhan, kami akan memberikan surat peringatan, jika perlu kami akan bawa ke pihak berwajib.</p>', 
 	    	attributes: {
 	    		'data-collapsed-icon': 'arrow-r',
 	    		'data-expanded-icon': 'arrow-d' 
@@ -42,7 +42,7 @@ function faq_page_page() {
 	    		'data-expanded-icon': 'arrow-d' 
 	    	} 
 	    },
-	    { header: 'Jam berapa layanan Get Tranz?', content: '<p>Jam layanan normal dimulai dari pukul 07:00 s/d 17:00. Khusus untuk layanan Get Transport dan Get Courier sampai dengan pukul 22:30. Dari Senin sampai Minggu, kecuali ada hari libur yang akan kami beritahukan lebih lanjut.</p>', 
+	    { header: 'Jam berapa layanan Get Tranz?', content: '<p>Jam layanan normal dimulai dari pukul 08:00 s/d 17:00. Dari Senin sampai Minggu, kecuali ada hari libur yang akan kami beritahukan lebih lanjut.</p>', 
 	    	attributes: {
 	    		'data-collapsed-icon': 'arrow-r',
 	    		'data-expanded-icon': 'arrow-d' 

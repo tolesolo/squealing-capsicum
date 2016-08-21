04625025e2646623c75c90f7539f5539e7bcf65d
tolesolo <blucky78@yahoo.com>
2016-08-21 23:07:14 +0700

    www

--- app/settings.js
+++ app/settings.js
@@ -15,7 +15,7 @@ Drupal.settings.debug = false;
 /* DRUPAL PATHS */
  
 // Site Path (do not use a trailing slash)
-Drupal.settings.site_path = 'http://gettranz.com'; // e.g. http://www.example.com
+Drupal.settings.site_path = 'http://www.gettranz.com'; // e.g. http://www.example.com
 
 // Default Services Endpoint Path
 Drupal.settings.endpoint = 'drupalgap';
